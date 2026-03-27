972f660fef5a1dc6f4575c92f92ec0d7684454ad
pfg <pfg@pfg.pw>
2026-03-27 14:42:18 -0700

    ,,wip

--- src/data/progression.ts
+++ src/data/progression.ts
@@ -7,66 +7,101 @@ const recipe_category = {
     crafting: id("recipe-category", "crafting"),
     growing: quickRecipeCategory("growing"),
     blending: quickRecipeCategory("blending"),
+    filling: quickRecipeCategory("blending"),
+};
+const group = {
+    main: itemGroup("main", "__farmoreos__/art/item-group.png", 32),
+};
+const subgroup = {
+    tools: itemSubgroup(group.main, "tools"),
+    seeds: itemSubgroup(group.main, "seeds"),
+    vegetables: itemSubgroup(group.main, "vegetables"),
+    research: itemSubgroup(group.main, "research"),
+};
+const fluid = {
+    water: id("fluid", "water"),
 };
 const item = {
     wood: id("item", "wood"),
     iron_plate: id("item", "iron-plate"),
     ice: id("item", "ice"),
-    kale_seeds: quickItem("kale-seeds", "__farmoreos__/art/kale-seeds.png", {}),
-    kale_leaf: quickItem("kale-leaf", "__farmoreos__/art/kale-leaf2.png", {contaminable: true, edible: 5}),
-    kale_smoothie: quickItem("kale-smoothie", "__farmoreos__/art/kale-smoothie.png", {contaminable: true, researchable: true}),
-    watering_can_empty: quickItem("watering-can-empty", "__farmoreos__/art/watering-can-empty.png", {}),
-    watering_can_full: quickItem("watering-can", "__farmoreos__/art/watering-can.png", {}),
-    hoe: quickItem("hoe", "__farmoreos__/art/hoe.png", {}),
+    kale_seeds: quickItem(subgroup.seeds, "kale-seeds", "__farmoreos__/art/kale-seeds.png", {}),
+    kale_leaf: quickItem(subgroup.vegetables, "kale-leaf", "__farmoreos__/art/kale-leaf2.png", {contaminable: true, edible: 5}),
+    kale_smoothie: quickItem(subgroup.research, "kale-smoothie", "__farmoreos__/art/kale-smoothie.png", {contaminable: true, researchable: true}),
+    watering_can_empty: quickItem(subgroup.tools, "watering-can-empty", "__farmoreos__/art/watering-can-empty.png", {}),
+    watering_can_full: quickItem(subgroup.tools, "watering-can", "__farmoreos__/art/watering-can.png", {}),
+    hoe: quickItem(subgroup.tools, "hoe", "__farmoreos__/art/hoe.png", {}),
+};
+const recipes = {
+    hoe: quickRecipe(subgroup.tools, "hoe", recipe_category.crafting, {
+        sec: 1,
+        from: [{name: item.iron_plate, amount: 20}, {name: item.wood, amount: 10}],
+        to: [{name: item.hoe, amount: 1}],
+    }),
+    watering_can_empty: quickRecipe(subgroup.tools, "watering-can-empty", recipe_category.crafting, {
+        sec: 1,
+        from: [{name: item.iron_plate, amount: 20}],
+        to: [{name: item.watering_can_empty, amount: 1}]
+    }),
+    watering_can_full: quickRecipe(subgroup.tools, "watering-can-full", recipe_category.crafting, {
+        sec: 1,
+        from: [{name: item.watering_can_empty, amount: 1}, {name: fluid.water, amount: 1}],
+        to: [{name: item.watering_can_empty, amount: 1}]
+    }),
+    kale_seeds: quickRecipe(subgroup.seeds, "kale-seeds", recipe_category.crafting, {
+        sec: 1,
+        from: [{name: item.wood, amount: 30}],
+        to: [{name: item.kale_seeds, amount: 1}],
+    }),
+    kale_leaf: quickRecipe(subgroup.vegetables, "kale-leaf", recipe_category.growing, {
+        sec: 2 * day_to_seconds,
+        from: [{name: item.kale_seeds, amount: 1}],
+        to: [
+            {name: item.kale_leaf, amount: 10},
+            {name: item.kale_seeds, amount: 1, extra_count_fraction: 0.1},
+        ],
+    }),
+    kale_smoothie: quickRecipe(subgroup.research, "kale-smoothie", recipe_category.blending, {
+        sec: 1,
+        from: [
+            {name: item.kale_leaf, amount: 10},
+            {name: item.ice, amount: 10},
+        ],
+        to: [{name: item.kale_smoothie, amount: 1}],
+    }),
 };
 
-const recipes = itemGroup("main", {
-    tools: itemSubgroup("tools", {
-        hoe_recipe: quickRecipe("hoe", recipe_category.crafting, {
-            sec: 1,
-            from: [{name: item.iron_plate, amount: 20}, {name: item.wood, amount: 10}],
-            to: [{name: item.hoe, amount: 1}],
-        }),
-        watering_can_empty: quickRecipe("watering-can-empty", recipe_category.crafting, {
-            sec: 1,
-            from: [{name: item.iron_plate, amount: 20}],
-            to: [{name: item.watering_can_empty, amount: 1}]
-        }),
-    }),
-    seeds: itemSubgroup("seeds", {
-        kale_seeds: quickRecipe("kale-seeds", recipe_category.crafting, {
-            sec: 1,
-            from: [{name: item.wood, amount: 30}],
-            to: [{name: item.kale_seeds, amount: 1}]
-        }),
-    }),
-    vegetables: itemSubgroup("vegetables", {
-        kale_leaf: quickRecipe("kale-leaf", recipe_category.growing, {
-            sec: 2 * day_to_seconds,
-            from: [{name: item.kale_seeds, amount: 1}],
-            to: [
-                {name: item.kale_leaf, amount: 10},
-                {name: item.kale_seeds, amount: 1, extra_count_fraction: 0.1},
-            ],
-        }),
-    }),
-    research: itemSubgroup("research", {
-        kale_smoothie: quickRecipe("kale-smoothie", recipe_category.blending, {
-            sec: 1,
-            from: [
-                {name: item.kale_leaf, amount: 10},
-                {name: item.ice, amount: 10},
-            ],
-            to: [{name: item.kale_smoothie, amount: 1}],
-        }),
-    }),
-})
-
 quickTechnology("basic-farming", {
+    essential: true,
+    icon: {path: "__farmoreos__/art/kale-leaf.png", size: 64},
+    cost: {
+        // TODO: this one should depend on mining 40 wood?
+        items: [id("item", "automation-science-pack")],
+        count: 1,
+        time: 1,
+    },
     unlocks: [
-        recipes.tools.watering_can_empty,
-        recipes.seeds.kale_seeds,
-        recipes.vegetables.kale_leaf,
-        recipes.research.kale_smoothie,
+        recipes.watering_can_empty,
+        recipes.watering_can_full,
+        recipes.hoe,
+        recipes.kale_seeds,
+        recipes.kale_leaf,
+        recipes.kale_smoothie,
+        // recipes.blender_base
+        // recipes.hand_blender
+        // recipes.ice
     ],
 });
+// quickTechnology("food-safety", {
+//     essential: false,
+//     icon: {path: "__farmoreos__/art/food-belt.png", size: 32},
+//     cost: {
+//         items: [item.kale_smoothie],
+//         count: 10,
+//         time: 10,
+//     },
+//     unlocks: [
+//         recipes.food_inserter,
+//         recipes.food_belt,
+//     ],
+// });
--- src/data/progression_impl.ts
+++ src/data/progression_impl.ts
@@ -1,10 +1,10 @@
 import { ActiveMods, FeatureFlags, PrototypeData } from "factorio:common";
-import { RecipeCategory, TechnologyPrototype } from "factorio:prototype";
+import { AnyPrototype, ItemGroup, ItemSubGroup, RecipeCategory, TechnologyPrototype } from "factorio:prototype";
 declare const data: PrototypeData;
 declare const mods: ActiveMods;
 declare const feature_flags: FeatureFlags;
 
-export type ID<T extends string> = {kind: T, name: string};
+export type ID<T extends string, Q = unknown> = {kind: T, name: string, prototype: Q};
 const namespace = "farmoreos";
 
 let next_order = 1000;
@@ -12,31 +12,66 @@ function nextOrder(): string {
     return tostring(next_order++);
 }
 
-export function id<T extends string>(kind: T, name: string): ID<NoInfer<T>> {
-    return {kind, name};
+export function id<T extends string>(kind: T, name: string): ID<NoInfer<T>, undefined> {
+    return {kind, name, prototype: undefined};
+}
+export function mkret<T extends string, U extends AnyPrototype>(kind: NoInfer<T>, name: string, value: NoInfer<U>): ID<T, U> {
+    data.extend([value]);
+    return {kind, name, prototype: value};
 }
 
-export function itemGroup<T extends {[key: string]: {[key: string]: ID<string>}}>(name: string, ch: T): NoInfer<T> {
+export function itemGroup(name: string, icon: string, icon_size: number): ID<"item-group", ItemGroup> {
     name = `${namespace}-${name}`;
-    return ch;
+    return mkret("item-group", name, {
+        type: "item-group",
+        name,
+        order: `z${name}`,
+        icon,
+        icon_size,
+    });
 }
-export function itemSubgroup<T extends {[key: string]: ID<string>}>(name: string, ch: T): NoInfer<T> {
+export function itemSubgroup(group: ID<"item-group">, name: string): ID<"item-subgroup", ItemSubGroup> {
     name = `${namespace}-${name}`;
-    return ch;
+    return mkret("item-subgroup", name, {
+        type: "item-subgroup",
+        name,
+        group: group.name,
+        order: nextOrder(),
+    });
 }
+
+export function quickRecipeCategory(name: string): ID<"recipe-category", RecipeCategory> {
+    name = `${namespace}-${name}`;
+    return mkret("recipe-category", name, {
+        type: "recipe-category",
+        name: name,
+        order: nextOrder(),
+    });
+}
+
 
 export function quickTechnology(name: string, opts: {
+    essential: boolean,
+    icon: {path: string, size: number},
+    cost: {items: ID<"item">[], count: number, time: number},
     unlocks: ID<string>[],
-}): ID<"technology"> {
+}): ID<"technology", TechnologyPrototype> {
     name = `${namespace}-${name}`;
-    data.extend([{
+    return mkret("technology", name, {
         type: "technology",
         name: name,
-    } satisfies TechnologyPrototype]);
-    return id("technology", name);
+        icon: opts.icon.path,
+        icon_size: opts.icon.size,
+        essential: opts.essential,
+        unit: {
+            count: opts.cost.count,
+            time: opts.cost.time,
+            ingredients: opts.cost.items.map(item => ([item.name, 1])),
+        },
+    });
 }
 
-export function quickItem(name: string, picture: string, attrs: {
+export function quickItem(subgroup: ID<"item-subgroup">, name: string, picture: string, attrs: {
     contaminable?: boolean,
     edible?: number,
     researchable?: boolean,
@@ -45,21 +80,11 @@ export function quickItem(name: string, picture: string, attrs: {
     return id("item", name);
 }
 
-export function quickRecipe(name: string, category: ID<"recipe-category">, opts: {
+export function quickRecipe(subgroup: ID<"item-subgroup">, name: string, category: ID<"recipe-category">, opts: {
     sec: number,
-    from: {name: ID<"item"> | string, amount: number}[],
-    to: {name: ID<"item"> | string, amount: number, extra_count_fraction?: number}[],
+    from: {name: ID<"item"> | ID<"fluid">, amount: number}[],
+    to: {name: ID<"item"> | ID<"fluid">, amount: number, extra_count_fraction?: number}[],
 }): ID<"recipe"> {
     name = `${namespace}-${name}`;
     return id("recipe", name);
-}
-
-export function quickRecipeCategory(name: string): ID<"recipe-category"> {
-    name = `${namespace}-${name}`;
-    data.extend([{
-        type: "recipe-category",
-        name: name,
-        order: nextOrder(),
-    } satisfies RecipeCategory]);
-    return id("recipe-category", name);
 }
